10067b80459c5128b479d4a041ae815da614e4c6
Luiggy102 <79861715+Luiggy102@users.noreply.github.com>
2024-11-06 11:34:20 -0500

    no icons for vim-dadbob-ui

--- lua/plugins/lista.lua
+++ lua/plugins/lista.lua
@@ -260,7 +260,7 @@ return {
 			"DBUIFindBuffer",
 		},
 		init = function()
-			vim.g.db_ui_use_nerd_fonts = 1
+			vim.g.db_ui_use_nerd_fonts = 0
 			vim.g.db_ui_win_position = "right"
 			vim.g.db_ui_winwidth = 35
 		end,
